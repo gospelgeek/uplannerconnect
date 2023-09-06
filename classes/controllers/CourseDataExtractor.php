8eccf39a76ccf7a0b3059a88d845d711895628c8
cristian machado <cristiank@cristiank.cristiank>
2023-09-06 03:11:50 -0400

    Progress in logic and implementation of the structure

--- classes/controllers/CourseDataExtractor.php
+++ classes/controllers/CourseDataExtractor.php
@@ -5,11 +5,43 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 namespace local_uplannerconnect;
+global $DB;
 
+$tableName = 'uplannerconnect_course';
 
 class CourseDataExtractor {
 
     public function __construct() {
+    }
+
+    /**
+     * @author Cristian Machado <cristian.machado@correounivalle.edu.co>
+     * @description Inserta un registro en la tabla miplugin_tabla 
+    */
+    public function insertCourseRegistry($data) {
+        
+        //Parámetros de ejemplo
+        $id_curso = 1;
+        $id_student = 123;
+        $item_note = 2;
+        $note = 'Nota de ejemplo';
+       
+         //Instancia de la base de datos
+         $record = new \stdClass();
+         $record->id_curso = $id_curso;
+         $record->id_student = $id_student;
+         $record->item_note = $item_note;
+         $record->note = $note;
+       
+          //Insertar el registro
+          $result = $DB->insert_record($tableName, $record);
+       
+          if ($result) {
+             $this->log('Registro insertado correctamente en miplugin_tabla.', \core\log\level::INFO);
+          } else {
+             $this->log('Error al insertar el registro en miplugin_tabla.', \core\log\level::ERROR);
+          }
+
     }
 
 }
