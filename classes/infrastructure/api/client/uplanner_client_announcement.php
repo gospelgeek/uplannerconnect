91966228c1e60d23cc5d39d471ebf2760e4724f1
Daniel Dorado <doradodaniel14@gmail.com>
2023-10-27 17:33:32 -0500

    Added uPlanner request

--- classes/infrastructure/api/client/uplanner_client_announcement.php
+++ classes/infrastructure/api/client/uplanner_client_announcement.php
@@ -2,22 +2,17 @@
 /**
  * @package     uPlannerConnect
  * @copyright   Cristian Machado Mosquera <cristian.machado@correounivalle.edu.co>
- * @copyright   daniel eduardo dorado <doradodaniel14@gmail.com>
+ * @copyright   Daniel Eduardo Dorado <doradodaniel14@gmail.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace local_uplannerconnect\infrastructure\api\client;
 
 /**
- * @package uPlannerConnect
- * @author Cristian Machado <cristian.machado@correounivalle.edu.co>
- * @author Daniel Dorado <doradodaniel14@gmail.com>
- * @description Cliente para consumir los métodos expuestos de anuncios en Uplanner
+ * uPlanner announcement client
  */
 class uplanner_client_announcement extends abstract_uplanner_client
 {
-    const ENDPOINT_MATERIAL = '/integration/announcements';
-
     /**
      * @inerhitdoc
      */
@@ -29,98 +24,7 @@ class uplanner_client_announcement extends abstract_uplanner_client
     protected string $email_subject = 'upllaner_email_subject_anounciente';
 
     /**
-     * Get announcement in Uplanner
-     *
-     * @param null $data
-     * @return array|bool|mixed|string
-     * @throws \Exception
+     * @inerhitdoc
      */
-    public function get($data = null)
-    {
-        $response = [];
-        if (!$this->get_token()) {
-            return $response;
-        }
-        $endpoint = $this->get_endpoint(self::ENDPOINT_MATERIAL);
-        $response = $this->curl_wrapper->get($endpoint);
-        if ($this->curl_wrapper->get_code() === 200) {
-            $response = json_decode($response, true);
-        } else {
-            $response['error'] = $this->curl_wrapper->get_error();
-        }
-
-        return $response;
-    }
-
-    /**
-     * Update announcement in Uplanner
-     *
-     * @param $data
-     * @return array
-     * @throws Exception
-     */
-    public function update($data): array
-    {
-        $response = [];
-        if (!$this->get_token()) {
-            return $response;
-        }
-        $endpoint = $this->get_endpoint(self::ENDPOINT_MATERIAL);
-        $response = $this->curl_wrapper->put($endpoint, $data);
-        if ($this->curl_wrapper->get_code() === 200) {
-            $response = json_decode($response, true);
-        } else {
-            $response['error'] = $this->curl_wrapper->get_error();
-        }
-
-        return $response;
-    }
-
-    /**
-     * Create announcement in Uplanner
-     *
-     * @param $data
-     * @return array
-     * @throws Exception
-     */
-    public function create($data): array
-    {
-        $response = [];
-        if (!$this->get_token()) {
-            return $response;
-        }
-        $endpoint = $this->get_endpoint(self::ENDPOINT_MATERIAL);
-        $response = $this->curl_wrapper->post($endpoint, $data);
-        if ($this->curl_wrapper->get_code() === 200) {
-            $response = json_decode($response, true);
-        } else {
-            $response['error'] = $this->curl_wrapper->get_error();
-        }
-
-        return $response;
-    }
-
-    /**
-     * Delete announcement in Uplanner
-     *
-     * @param $data
-     * @return array
-     * @throws Exception
-     */
-    public function delete($data = null): array
-    {
-        $response = [];
-        if (!$this->get_token()) {
-            return $response;
-        }
-        $endpoint = $this->get_endpoint(self::ENDPOINT_MATERIAL);
-        $response = $this->curl_wrapper->delete($endpoint);
-        if ($this->curl_wrapper->get_code() === 200) {
-            $response = json_decode($response, true);
-        } else {
-            $response['error'] = $this->curl_wrapper->get_error();
-        }
-
-        return $response;
-    }
+    protected string $config_topic = 'announcements_endpoint';
 }
--- lang/es/local_uplannerconnect.php
+++ lang/es/local_uplannerconnect.php
@@ -35,15 +35,43 @@ $string['title'] = 'uPlannerConnect';
 $string['uplannerconnect:index'] = 'uPlannerConnect';
 $string['controllerpath'] = 'Ruta del controlador:';
 $string['controllerpathdesc'] = 'Especifica la ruta del controlador.';
-$string['courses_endpoint_heading'] = 'Configuración del endpoint de cursos';
-$string['courses_endpoint'] = 'URL del endpoint de cursos';
-$string['courses_endpoint_desc'] = 'Ingrese la URL del endpoint de cursos.';
-$string['notifications_endpoint_heading'] = 'Configuración del endpoint de notificaciones';
-$string['notifications_endpoint'] = 'URL del endpoint de notificaciones';
-$string['notifications_endpoint_desc'] = 'Ingrese la URL del endpoint de notificaciones.';
-$string['materials_endpoint_heading'] = 'Configuración del endpoint de materiales';
-$string['materials_endpoint'] = 'URL del endpoint de materiales';
-$string['materials_endpoint_desc'] = 'Ingrese la URL del endpoint de materiales.';
+
+// uPlanner key field
+$string['key_heading'] = 'Configuración de la key de uPlanner';
+$string['key'] = 'uPlanner key';
+$string['key_desc'] = 'Ingrese la key de uPlanner';
+
+// uPlanner base url field
+$string['base_url_heading'] = 'Configuración del base url de  uPlanner';
+$string['base_url'] = 'Base url de uPlanner';
+$string['base_url_desc'] = 'Ingrese el base url de uPlanner';
+
+// uPlanner token field
+$string['token_endpoint_heading'] = 'Configuración de la url para el token';
+$string['token_endpoint'] = 'Url del token';
+$string['token_endpoint_desc'] = 'Ingrese la url para el endpoint del token.';
+
+// uPlanner materials field
+$string['materials_endpoint_heading'] = 'Configuración del topic para materials';
+$string['materials_endpoint'] = 'Topic de materials';
+$string['materials_endpoint_desc'] = 'Ingrese el topic para el endpoint de materiales.';
+
+// uPlanner announcements field
+$string['announcements_endpoint_heading'] = 'Configuración del topic para anuncios';
+$string['announcements_endpoint'] = 'Topic de anuncios';
+$string['announcements_endpoint_desc'] = 'Ingrese el topic para el endpoint de anuncios.';
+
+// uPlanner evaluation_structure field
+$string['evaluation_structure_endpoint_heading'] = 'Configuración del topic para evaluaciones';
+$string['evaluation_structure_endpoint'] = 'Topic de evaluaciones';
+$string['evaluation_structure_endpoint_desc'] = 'Ingrese el topic para el endpoint de evaluaciones.';
+
+// uPlanner grades field
+$string['grades_endpoint_heading'] = 'Configuración del topic para notas';
+$string['grades_endpoint'] = 'Topic de notas';
+$string['grades_endpoint_desc'] = 'Ingrese el topic para el endpoint de notas.';
+
+// uPlanner task rutine field
 $string['task_rutine_primary'] = 'Tiempo de ejecución de la tarea principal';
 $string['task_rutine_primary_desc'] = 'Ingrese el tiempo de ejecución en segundos para la tarea principal.';
 
